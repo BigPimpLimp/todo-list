868d92b0c27e151898e64007d7d0f61ec0a401f3
bigpimplimp <levi.jcecil@gmail.com>
2025-01-24 12:07:38 -0800

    create task class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('Yeet');
+
+export class task {
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+
+
+    
+}
+
