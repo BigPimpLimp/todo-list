663b8f1c57676badbf61f5712fa613168639a70c
bigpimplimp <levi.jcecil@gmail.com>
2025-02-05 14:04:30 -0800

    add delete item function (in progress), update display logic (in progress)

--- src/dom.js
+++ src/dom.js
@@ -11,13 +11,16 @@ export function displayInputWindow(i) {
 
 (function displayStoredTask() {
     const item = fetchItem('tasklist');
+    if (item == null) {
+        return
+    }
     let i = 0;
     let exec = false;
+    console.log(item)
     item.forEach(e => {
         for (let key in e) {
             if (Object.keys(e).slice(-1)[0] !== key) {
-
-            
+                
             if (!exec) {
             createElement('task-list', 'div', '', 'task-div', 'div' + i)
             exec = true
@@ -29,6 +32,30 @@ export function displayInputWindow(i) {
         exec = false;
     });
 })();
+
+export function displayStoredTask() {
+    const item = fetchItem('tasklist');
+    if (item == null) {
+        return
+    }
+    let i = 0;
+    let exec = false;
+    console.log(item)
+    item.forEach(e => {
+        for (let key in e) {
+            if (Object.keys(e).slice(-1)[0] !== key) {
+                
+            if (!exec) {
+            createElement('task-list', 'div', '', 'task-div', 'div' + i)
+            exec = true
+            }
+            createElement('div' + i, 'p', charRemove(JSON.stringify(e[key])), 'task-p')
+            }
+        }
+        ++i; 
+        exec = false;
+    });
+};
 
 let i = 0;
 
@@ -53,3 +80,13 @@ export function clearForm() {
     }
 }
 
+(function createDeleteButton() {
+    const taskDiv = document.querySelectorAll('.task-div')
+    taskDiv.forEach(task => {
+        const btn = document.createElement('button')
+        btn.textContent = 'delete'
+        btn.setAttribute('class', 'delete-btn')
+        task.appendChild(btn)
+    })
+
+})()
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
-import { displayInputWindow, displayNewTask, clearForm } from './dom';
+import { displayInputWindow, displayNewTask, clearForm, displayStoredTask } from './dom';
 
 console.log('Yeet');
+
+const taskArray = []
 
 export class task {
     static counter = 0;
@@ -11,7 +13,6 @@ export class task {
         this.dueDate = dueDate;
         this.priority = priority;
         this.notes = notes;
-        this.id = task.counter++;
     }  
 }
 
@@ -23,7 +24,7 @@ export class task {
     const submitBtn = document.getElementById('submit-btn');
     submitBtn.addEventListener('click', (e) => {
         const newTask = createTask();
-        storeItem('tasklist', newTask);
+        storeItem('tasklist', newTask, taskArray);
         displayNewTask(newTask)
         displayInputWindow(false);
         clearForm()
@@ -34,6 +35,21 @@ export class task {
         displayInputWindow(false)
         clearForm()
         e.preventDefault()
+    })
+    const deleteBtn = document.querySelectorAll('.delete-btn')
+    deleteBtn.forEach(button => {
+        button.addEventListener('click', (e) => {
+           const target = e.target.closest('.delete-btn')
+           console.log(target)
+           if (target) {
+            let id = e.target.parentElement.id
+            id = id.slice(3)
+            id = parseInt(id)
+            
+            console.log(id)
+            deleteItem('tasklist', id)
+           }
+        })
     })
 })();
 
@@ -49,21 +65,29 @@ export function createElement(id, element, value, cssClass, ownId) {
 
 function createTask() {
     const newTask = new task(document.getElementById('title').value, document.getElementById('description').value, document.getElementById('datePicker').value, document.getElementById('priority').value, '')
+    taskArray.push(newTask)
     return newTask
 }
 
-function storeItem(key, value) {
+function storeItem(key, value, arr) {
     const taskArray = fetchItem(key)
+    if (taskArray == null) {
+        localStorage.setItem(key, JSON.stringify(arr));
+        return
+    }
     taskArray.push(value)
     localStorage.setItem(key, JSON.stringify(taskArray));
 }
 
 function deleteItem(key, index) {
-    const taskArray = fetchItem(key)
-    taskArray.splice(index, 1)
+    let taskArray = fetchItem(key)
+    console.log(taskArray)
+    taskArray = taskArray.splice(index, 1)
+
     localStorage.setItem(key, JSON.stringify(taskArray));
 }
 
 export function fetchItem(key) {
     return JSON.parse(localStorage.getItem(key));
  }
+
